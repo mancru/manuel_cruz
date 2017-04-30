9ab231156025d8f2d7057ee26b538288623a6c40
mancru <mancrufor@alum.us.es>
2017-04-30 17:37:57 +0200

    Actuaciones sobre los comentarios de la tarea 1.
    Revertido al uso de if...else if y eliminado espacio y comentarios sobrantes.

--- main.c
+++ main.c
@@ -13,7 +13,6 @@ int world_count_neighbors(/* Recibo un mundo y unas coordenadas */);
 bool world_get_cell(/* Recibo un mundo y unas coordenadas */);
 void world_copy(/* Recibo dos mundos */);
 
-
 void world_init(bool w_x[W_SIZE_X][W_SIZE_Y])
 {
 	
@@ -22,15 +21,12 @@ void world_init(bool w_x[W_SIZE_X][W_SIZE_Y])
 			w_x[i][j]=DEAD;
 		}
 	}
-
 	/* Patrón del glider: */
-	
 	w_x[0][1]=ALIVE;
 	w_x[1][2]=ALIVE;
 	w_x[2][0]=ALIVE;
 	w_x[2][1]=ALIVE;
 	w_x[2][2]=ALIVE;
-	
 }
 
 void world_print(bool w_x[W_SIZE_X][W_SIZE_Y])
@@ -50,7 +46,6 @@ void world_print(bool w_x[W_SIZE_X][W_SIZE_Y])
 void world_step(bool w_1[W_SIZE_X][W_SIZE_Y], bool w_2[W_SIZE_X][W_SIZE_Y])
 {
 
-	// TODO: Recorrer array contando vecinas
 	int v;
 	bool cell;
 
@@ -61,12 +56,12 @@ void world_step(bool w_1[W_SIZE_X][W_SIZE_Y], bool w_2[W_SIZE_X][W_SIZE_Y])
 			w_2[i][j] = (v==3) || (cell && v==2);
 		}
 	}
-
 	world_copy(w_2, w_1);
 }
 
 int world_count_neighbors(bool w_x[W_SIZE_X][W_SIZE_Y], int x, int y)
 {
+
 	int count=0;
 
 	count += world_get_cell(w_x, x - 1, y - 1);
@@ -77,8 +72,7 @@ int world_count_neighbors(bool w_x[W_SIZE_X][W_SIZE_Y], int x, int y)
 	count += world_get_cell(w_x, x + 1, y - 1);
 	count += world_get_cell(w_x, x + 1, y	 );
 	count += world_get_cell(w_x, x + 1, y + 1);
-
-
+	
 	return count;
 }
 
@@ -88,16 +82,19 @@ int world_count_neighbors(bool w_x[W_SIZE_X][W_SIZE_Y], int x, int y)
 bool world_get_cell(bool w_x[W_SIZE_X][W_SIZE_Y], int x, int y)
 {
 
-	// TODO:
-	// if (x < 0)
-	// ...
-	// else if (x >= W_SIZE_X)
-	// ...
-
 	bool cell;
 
-	x<0 ? (x=W_SIZE_X-1) : ((x >= W_SIZE_X) ? x=0 : x);
-	y<0 ? (y=W_SIZE_Y-1) : ((y >= W_SIZE_Y) ? y=0 : y);
+	if (x < 0){
+		x=W_SIZE_X-1;
+	} else if (x >= W_SIZE_X){
+		x=0;
+	}
+
+	if (y < 0){
+		y=W_SIZE_Y-1;
+	} else if (y >= W_SIZE_Y){
+		y=0;
+	}
 
 	cell=w_x[x][y];
 
@@ -117,25 +114,14 @@ void world_copy(bool w_y[W_SIZE_X][W_SIZE_Y], bool w_x[W_SIZE_X][W_SIZE_Y])
 int main()
 {
 	int i = 0;
-
 	bool w_1[W_SIZE_X][W_SIZE_Y];
-
 	bool w_2[W_SIZE_X][W_SIZE_Y];
 
 	world_init(w_1);
-
 	do {
 		printf("\033cIteration %d\n", i++);
-
 		world_print(w_1);
-
 		world_step(w_1, w_2);
-
 	} while (getchar() != 'q');
-
 	return EXIT_SUCCESS;
 }
-
-
-
-
