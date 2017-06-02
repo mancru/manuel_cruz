d2928e59f7e4a8bc7ebf33f759bc61dce7fa8bf0
mancru <mancrufor@alum.us.es>
2017-06-02 11:57:50 +0200

    Eliminadas lçineas y comentarios sobrantes.

--- main.c
+++ main.c
@@ -4,13 +4,11 @@
 #include "world.h"
 #include "world_limited.h"
 #include "world_toroidal.h"
-
 #include "config.h"
 
 int main(int argc, char *argv[])
 {
 	struct config config;
-	struct world_limited;
 	struct world * world;
 
 	if (!config_parse_argv(&config, argc, argv)){
@@ -27,15 +25,12 @@ int main(int argc, char *argv[])
 
 	if (borders == CFB_DEFAULT ||
 		borders == CFB_TOROIDAL){
-
-		w = world_toroidal_alloc(&config);
-		if (!w){
-			perror("Can't allocate world");
-			exit(EXIT_FAILURE);
-		}
-
+			w = world_toroidal_alloc(&config);
+			if (!w){
+				perror("Can't allocate world");
+				exit(EXIT_FAILURE);
+			}
 	} else if (borders == CFB_LIMITED){
-
 		w = world_limited_alloc(&config);
 		if (!w){
 			perror("Can't allocate limited world");
@@ -46,18 +41,13 @@ int main(int argc, char *argv[])
 	int iIteracion = 0;
 	do {
 		printf("\033cIteration %d\n", iIteracion++);
-		config_print(&config);
 		world_print(w);
 		world_iterate(w);
 	} while (getchar() != 'q');
 
 	if (w){
-		//if (borders == CFB_TOROIDAL){ 
-			//world_toroidal_free(w);
-		//} else {
-			world=(struct world *)w;
-			world->world_free(w);
-		//}
+		world=(struct world *)w;
+		world->world_free(w);
 	}
 	return EXIT_SUCCESS;
 }
--- world.c
+++ world.c
@@ -6,15 +6,12 @@
 
 struct world;
 
-//static void fix_coords(const struct world *w, int *x, int *y);
-//static bool get_cell(const struct world *w, int x, int y);
-//static void set_cell(struct world *w, int buf, int x, int y, bool val);
 static int count_neighbors(const struct world *w, int x, int y);
 
 struct world *world_alloc(struct config * config)
 {
 	struct world * w;
-	w = (struct world *)malloc(sizeof(struct world)); //error
+	w = (struct world *)malloc(sizeof(struct world));
 	if (!w)
 		return NULL;
 
@@ -26,24 +23,20 @@ void world_init(struct config * config, struct world * w)
 	w->size_x = config->size_x;
 	w->size_y = config->size_y;
 
-	bool * w1;
-	w1 = (bool *)malloc(w->size_x*w->size_y*sizeof(bool)); //error
-	if (!w1){
-		free(w);
+	w->cells[0] = (bool *)malloc(w->size_x*w->size_y*sizeof(bool));
+	if (!w->cells[0]){
+		free(w->cells[0]);
 		perror("World 1 could not be allocated!\n");
 		exit(-11);
 	}
-	bool * w2;
-	w2 = (bool *)malloc(w->size_x *w->size_y*sizeof(bool)); //error
-	if (!w2){
-		free(w1);
+
+	w->cells[1] = (bool *)malloc(w->size_x *w->size_y*sizeof(bool));
+	if (!w->cells[1]){
+		free(w->cells[0]);
 		free(w);
 		perror("World 2 could not be allocated!\n");
 		exit(-12);
 	}
-
-	w->cells[0] = w1;
-	w->cells[1] = w2;
 
 	//Punteros a las funciones
 	w->world_alloc = world_alloc;
@@ -51,8 +44,6 @@ void world_init(struct config * config, struct world * w)
 	w->world_iterate = world_iterate;
 	w->world_print = world_print;
 	w->count_neighbors = count_neighbors;
-//	w->set_cell = set_cell;//antes set_cell
-//	w->get_cell = get_cell;//antes get_cell
 
 	//Inicialización del mundo
 	for (int i=0;i<w->size_x;i++){
@@ -124,37 +115,7 @@ static int count_neighbors(const struct world *w, int x, int y)
 	count += w->get_cell (w, x + 1, y + 1);
 	return count;
 }
-/*
-static void set_cell(struct world *w, int buf, int x, int y, bool val)
-{
-	* (w->cells[buf] + x*(w->size_y)+y) = val;
-}
 
-static bool get_cell(const struct world *w, int x, int y)
-{
-	bool cell;
-	int * ptrx = &x;
-	int * ptry = &y;
-	fix_coords(w, ptrx, ptry);
-	cell = * (w->cells[0] + x*(w->size_y)+y);
-	return cell;
-}
-
-static void fix_coords(const struct world *w, int *x, int *y)
-{
-	if (*x < 0){
-		*x=(w->size_x)-1;
-	} else if ( *x >= (w->size_x) ){
-		*x=0;
-	}
-
-	if (*y < 0){
-		*y=(w->size_y)-1;
-	} else if ( *y >= (w->size_y) ){
-		*y=0;
-	}
-}
-*/
 void world_free(struct world *w)
 {
 	free(w->cells[0]);
