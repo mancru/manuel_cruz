fcd59649572d889e216f1b015b83368dc477ed20
mancru <mancrufor@alum.us.es>
2017-06-09 14:52:00 +0200

    Borrado de tarea 7 en carpeta

--- tarea7_conErrores/world_limited.h
+++
@@ -1,9 +0,0 @@
-#ifndef _world_limited_h_
-#define _world_limited_h_
-#include "world_init.h"
-
-/* Public methods */
-struct world_limited * world_limited_alloc();
-void world_limited_free(struct world_limited * wl);
-
-#endif
--- tarea7_conErrores/world_toroidal.h
+++
@@ -1,9 +0,0 @@
-#ifndef _world_toroidal_h_
-#define _world_toroidal_h_
-#include "world_init.h"
-
-/* Public methods */
-struct world_toroidal * world_toroidal_alloc(struct config * config);
-void world_toroidal_free(struct world_toroidal *wt);
-
-#endif
